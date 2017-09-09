5a52ea689a7a8e1be788599d24df010dd414681d
akicho8 <akicho8@gmail.com>
2017-09-09 21:05:07 +0900

    Update README

--- examples/0240_attr_reader_option.rb
+++ examples/0240_attr_reader_option.rb
@@ -10,32 +10,32 @@ class C1
   end
 end
 
-C1.first.x rescue $! # => #<NoMethodError: undefined method `x' for #<C1:0x007fb2c710eda8>>
-C1.first.y rescue $! # => #<NoMethodError: undefined method `y' for #<C1:0x007fb2c710eda8>>
-C1.first.z rescue $! # => #<NoMethodError: undefined method `z' for #<C1:0x007fb2c710eda8>>
+C1.first.x rescue $! # => #<NoMethodError: undefined method `x' for #<C1:0x007ff0338d65c8>>
+C1.first.y rescue $! # => #<NoMethodError: undefined method `y' for #<C1:0x007ff0338d65c8>>
+C1.first.z rescue $! # => #<NoMethodError: undefined method `z' for #<C1:0x007ff0338d65c8>>
 
 class C2
   include MemoryRecord
-  memory_record attr_reader: {only: :x} do
+  memory_record attr_reader: {only: :y} do
     [
       {x: 1, y: 1, z: 1},
     ]
   end
 end
 
-C2.first.x rescue $! # => 1
-C2.first.y rescue $! # => #<NoMethodError: undefined method `y' for #<C2:0x007fb2c70e6df8>>
-C2.first.z rescue $! # => #<NoMethodError: undefined method `z' for #<C2:0x007fb2c70e6df8>>
+C2.first.x rescue $! # => #<NoMethodError: undefined method `x' for #<C2:0x007ff0338ae780>>
+C2.first.y rescue $! # => 1
+C2.first.z rescue $! # => #<NoMethodError: undefined method `z' for #<C2:0x007ff0338ae780>>
 
 class C3
   include MemoryRecord
-  memory_record attr_reader: {except: :x} do
+  memory_record attr_reader: {except: :y} do
     [
       {x: 1, y: 1, z: 1},
     ]
   end
 end
 
-C3.first.x rescue $! # => #<NoMethodError: undefined method `x' for #<C3:0x007fb2c70ce668>>
-C3.first.y rescue $! # => 1
+C3.first.x rescue $! # => 1
+C3.first.y rescue $! # => #<NoMethodError: undefined method `y' for #<C3:0x007ff033895e88>>
 C3.first.z rescue $! # => 1
